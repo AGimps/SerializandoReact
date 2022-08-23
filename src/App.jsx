09078bda1f68e673206b7a30ab0c2cbe9b790c38
Ariel Gimpel <67701240+AGimps@users.noreply.github.com>
2022-08-23 10:41:51 -0300

    Arquivos base Serializando
    adicionado arquivos base.

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import React from "react";
+import PaginaInicial from './componentes/login.jsx';
+
+export default function App() {
+  return (
+    <PaginaInicial />
+  );
+  
+}
